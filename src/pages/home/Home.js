47164e08c7a18c8f9f5dcb348f5aa3b9b1b8228b
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2025-09-14 23:34:07 +0530

    Added codes for Card and SpecialDishes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Banner from '../../Components/Banner'
+import Categories from './Categories'
+import SpecialDishes from './SpecialDishes'
 
 function Home() {
     return (
         <div>
             <Banner />
+            <Categories />
+            <SpecialDishes />
         </div>
     )
 }
