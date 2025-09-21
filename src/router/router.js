986791859ea40dbac46681ef3ea774db526446c1
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2025-09-21 19:16:22 +0530

    Added codes for Menu.js and updated codes under main.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/main";
 import Home from "../pages/home/Home";
+import Menu from "../pages/shop/Menu";
 
 const router = createBrowserRouter([
     {
@@ -9,11 +10,11 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home />                
+                element: <Home />
             },
             {
-                path: '/shop',
-                
+                path: '/menu',
+                element: <Menu />
             }
         ]
     }
